0af45551742f5b2c5b0e3c2146622f07ef2ba482
tukutozukarin <31255753+Tukutozukarin@users.noreply.github.com>
2020-02-25 10:23:38 +0100

    navigation bar

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ import Mr from './components/pages/Mr';
 import SplitPane from 'react-split-pane';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 import logo from './img/questbacklogo.png';
+import iconCalculator from './img/iconcalculator.png';
 
 import './App.css';
 
 const sideNavStyles = {
   background: '#6D9BB5',
-  width: '7%',
+  width: '10%',
   cursor: 'col-resize',
   margin: '0 0.1px',
   height: '100%',
@@ -39,23 +39,38 @@ const App = () => {
 
 
         <menu class="container-fluid">
-        <div class="container-fluid col-xs-12 col-md-8"><img src={logo}  position="absolute" alt="questbackLogo" width="115px" height="40px" /></div>
+
+
+        <div class="container-fluid col-xs-12 col-md-8"><img src={logo}  
+        position="absolute" alt="questbackLogo" width="115px" height="40px" /></div>
         <div class="container-fluid col-xs-12 col-md-8"><h3><p></p></h3></div>
 
-          <div class="container-fluid col-xs-12 col-md-8"><Link to="/" position="absolute" style={{ color: '#000000', fontSize: '28px', fontWeight: 'bold' }}>MR</Link>
-            <div class="container-fluid col-xs-12 col-md-8"><Link to="/" position="absolute" style={{ color: '#000000', fontSize: '17px' }}>IpsumLipsum</Link></div>
-              <div class="container-fluid col-xs-12 col-md-8"><Link to="/" position="absolute" style={{ color: '#000000', fontSize: '17px' }}>Mipsum</Link></div>
+
+      <div><text><img src={iconCalculator}  
+        position="absolute" alt="iconCalculator" width="20px" height="20px" 
+        style={{display : 'inline-block', color: '#000000', fontSize: '28px', fontWeight: 'bold'}} />
+        <Link to="/"  style={{ color: '#000000', fontSize: '17px', fontWeight: 'bold' }}> ROI </Link>
+        </text>
+        </div>
+        <div class="container-fluid col-xs-12 col-md-8"><Link to="/" style={{ color: '#000000', fontSize: '17px'}}>Dashboard <p></p></Link></div>
+        <div class="container-fluid col-xs-12 col-md-8"><Link to="/" style={{ color: '#000000', fontSize: '17px'}}>Hidden <p></p></Link></div>
+        <div class="container-fluid col-xs-12 col-md-8"><Link to="/" style={{ color: '#000000', fontSize: '17px'}}>Internal <p></p></Link></div>
+        <div class="container-fluid col-xs-12 col-md-8"><Link to="/" style={{ color: '#000000', fontSize: '17px'}}>International int <p></p></Link></div>
+
+
+        <div class="container-fluid col-xs-12 col-md-8"><h3><p></p></h3>
+        </div>
+
+
+          <div class="container-fluid col-xs-12 col-md-8"><Link to="/"  style={{ color: '#000000', fontSize: '28px', fontWeight: 'bold' }}>MR</Link>
               <div class="container-fluid col-xs-12 col-md-8"><h3><p></p></h3></div>
           </div>
+
           <div class="container-fluid col-xs-12 col-md-8"><Link to="/ex" style={{ color: '#000000', fontSize: '28px', fontWeight: 'bold'  }}>EX</Link>
-            <div class="container-fluid col-xs-12 col-md-8"><Link to="/" style={{ color: '#000000', fontSize: '17px' }}>IpsumLipsum</Link></div>
-              <div class="container-fluid col-xs-12 col-md-8"><Link to="/" style={{ color: '#000000', fontSize: '17px' }}>Mipsum</Link></div>
               <div class="container-fluid col-xs-12 col-md-8"><h3><p></p></h3></div>
 
           </div>
           <div class="container-fluid col-xs-12 col-md-8"><Link to="/cx" style={{ color: '#000000', fontSize: '28px', fontWeight: 'bold'  }}>CX</Link>
-            <div class="container-fluid col-xs-12 col-md-8"><Link to="/" style={{ color: '#000000', fontSize: '17px' }}>IpsumLipsum</Link></div>
-              <div class="container-fluid col-xs-12 col-md-8"><Link to="/" style={{ color: '#000000', fontSize: '17px' }}>Mipsum</Link></div>
                 <div class="container-fluid col-xs-12 col-md-8"><h3><p></p></h3></div>
           </div>
         </menu>
